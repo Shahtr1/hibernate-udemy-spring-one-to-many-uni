cc15408f31ab153e7f9451bc8528d34c5edf0e3e
Shahtr1 <tramboos123@gmail.com>
2021-03-09 14:40:02 +0530

    done

---
+++ src/com/luv2code/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,67 @@
+package com.luv2code.hibernate.demo;
+
+import java.text.ParseException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) throws ParseException {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			int theid = 1;
+			Instructor tempInstructor = 
+					session.get(Instructor.class, theid);
+			
+			System.out.println("luv2code: Instructor: " + tempInstructor);
+			
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			// close the session
+			session.close();
+			
+			System.out.println("\nluv2code: The session is now closed!\n");
+			
+			// option 1: call getter method whle session is open
+			
+			
+			// get course for the instructor
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+			
+			System.out.println("luv2code: Done!");
+			
+			
+		}finally {
+			
+			session.close();
+			
+			factory.close();
+		}
+		
+	}
+
+}
---
+++ src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,57 @@
+package com.luv2code.hibernate.demo;
+
+import java.text.ParseException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorCoursesDemo {
+
+	public static void main(String[] args) throws ParseException {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			int theid = 1;
+			Instructor tempInstructor = 
+					session.get(Instructor.class, theid);
+			
+			System.out.println("Instructor: " + tempInstructor);
+			
+			// get course for the instructor
+			System.out.println("Courses: " + tempInstructor.getCourses());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+			
+		}finally {
+			
+			session.close();
+			
+			factory.close();
+		}
+		
+	}
+
+}
